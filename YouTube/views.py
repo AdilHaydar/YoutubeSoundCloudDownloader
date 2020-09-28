9585a71275f150f4f94a67f673b7796ecf7c3084
adil haydar <adilhaydarr@gmail.com>
2020-09-28 21:11:55 +0300

    Added Instagram video downloader

--- YouTube/views.py
+++ YouTube/views.py
@@ -4,6 +4,10 @@ import os
 from pytube import YouTube
 import shutil
 from moviepy.editor import *
+from bs4 import BeautifulSoup
+from selenium.webdriver.firefox.options import Options
+from selenium import webdriver
+import requests
 # Create your views here.
 
 
@@ -20,9 +24,8 @@ def index(request):
 		elif request.GET.get("comboBox") == "480p":
 			q_480p = yt.streams.filter(res="480p",mime_type="video/mp4").first().download()
 		elif request.GET.get("comboBox") == "audio":		
-			q_720p = yt.streams.filter(res="720p",mime_type="video/mp4").first().download()
 			
-
+			q_720p = yt.streams.filter(res="720p",mime_type="video/mp4").first().download()
 		
 		
 		for i in os.listdir(os.getcwd()):
@@ -36,6 +39,7 @@ def index(request):
 			os.remove(os.path.join(os.getcwd(),"media",downloaded_video))
 
 		data = {
+			"show":True,
 			"success":"Videoyu Başarılı Bir Şekilde İndirilebilirsiniz.",
 			"url":downloaded_video,
 			"title":yt.title,
@@ -58,4 +62,47 @@ def index(request):
 			"title":downloaded_music[:-4],
 		}
 		return render(request,'index.html',data)
+
+	elif request.GET.get("url") and "instagram.com" in request.GET.get("url").lower():
+		options = Options()
+		options.add_argument('-headless')
+		browser = webdriver.Firefox(executable_path=r"C:\Users\adilc\Desktop\geckodriver.exe")
+		browser.get(request.GET.get("url"))
+		
+		page_source = browser.page_source
+
+		soup = BeautifulSoup(page_source,'html.parser')
+
+		video_tag = soup.find('video',attrs={'class':'tWeCl'})
+		thumbnail = soup.find('img', attrs={'class':'_8jZFn'})
+		text_post = soup.find('div', attrs={'class':'C4VMK'})
+
+		text_post = text_post.find('span', attrs={'class':''})
+		text_post = text_post.text
+		thumbnail_url = thumbnail.get('src')
+		download_url = video_tag.get('src')
+		acc = soup.find('a', attrs={'class':'sqdOP'})
+		acc = acc.text
+		file_name = f"{acc}_ig_video.mp4"
+
+		with requests.get(download_url, stream = True) as r:
+			r.raise_for_status()
+			with open(file_name, 'wb') as f:
+				for chunk in r.iter_content(chunk_size=8192):
+					f.write(chunk)
+		for i in os.listdir(os.getcwd()):
+			if '.mp4' in i:
+				downloaded_video = i
+		shutil.move(downloaded_video,os.getcwd()+'/media/')
+		data = {
+			"show":True,
+			"success":"Videoyu Başarılı Bir Şekilde İndirilebilirsiniz.",
+			"url":downloaded_video,
+			"thumbnail":thumbnail_url,
+			"description":text_post,
+			"author":acc,
+		}
+
+		return render(request,'index.html',data)
 	return render(request,'index.html')
+
